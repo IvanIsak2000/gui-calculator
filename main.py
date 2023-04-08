9ade1f7d0bb19da6635749e1e22a7ec389cf6e56
IvanIsak2000 <vingving648@gmail.com>
2023-04-08 21:32:05 +0300

    add gui terminal

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import dearpygui.dearpygui as dpg 
 
 dpg.create_context()
+dpg.create_viewport()
 dpg.setup_dearpygui()
-dpg.create_viewport(title='Calculator', width=450, height=500)
 
 with dpg.theme() as primarybtn_green_theme:
     with dpg.theme_component(dpg.mvButton):
@@ -16,82 +16,83 @@ with dpg.theme() as primarybtn_red_theme:
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 6, category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_Button, (155, 0, 0), category=dpg.mvThemeCat_Core)
         dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, (0, 0, 0), category=dpg.mvThemeCat_Core)
-        
+
 elements = []
 
 def calculate(symbol: str):
     global elements
-    
     try:
         if symbol == '=':
-
-            e = elements
             global result 
-            result = eval("".join(str(i) for i in e ))
+            result = eval("".join(str(i) for i in elements ))
             elements.clear()
-            os.system('cls')
-            return print(result)
+            return dpg.set_value("input", result)
         
         elif symbol =='clear':
-
             elements.clear()
-            os.system('cls')
+            dpg.set_value("input", 0)
 
         elif symbol == 'del':
             elements = elements[:-1]
-            os.system('cls')
-            print("".join(str(i) for i in elements))
+            dpg.set_value("input", "".join(str(i) for i in elements))
 
         elif symbol.isdigit():
             elements.append(symbol)
-            os.system('cls')
-            print("".join(str(i) for i in elements))
+            dpg.set_value("input", "".join(str(i) for i in elements))
             
         else:
             elements.append(symbol)
-            os.system('cls')
-            print("".join(str(i) for i in elements))
+            dpg.set_value("input", "".join(str(i) for i in elements))
             
     except Exception as err:
-        return print(err)
+        return dpg.set_value("input", err)
 
-        
-with dpg.window(label="",tag = 'Primary Window'):
+with dpg.window() as main_window:
+    dpg.add_text("0", tag="input")
+    dpg.add_spacer(height=10)
+    with dpg.group(horizontal= True):
+        dpg.add_button(label="7", width=100, height = 100, callback = calculate, tag = '7')
+        dpg.add_button(label="8", width=100, height = 100, callback = calculate, tag = '8')
+        dpg.add_button(label="9", width=100, height = 100, callback = calculate, tag = '9')
+        dpg.add_button(label="%", width=100, height = 100, callback = calculate , tag = '%')
+        dpg.add_button(label="root", width=100, height = 100, callback = calculate , tag = '**0.5')
 
     with dpg.group(horizontal= True):
-        dpg.add_button(label="1", width=100, height = 80, callback = calculate, tag = '1')
-        dpg.add_button(label="2", width=100, height = 80,callback = calculate, tag = '2')
-        dpg.add_button(label="3", width=100, height = 80, callback = calculate, tag = '3')
-        dpg.add_button(label="+", width=100, height = 80, callback = calculate , tag = '+')
+        dpg.add_button(label="4", width=100, height = 100, callback = calculate, tag ='4')
+        dpg.add_button(label="5", width=100, height = 100, callback = calculate, tag ='5')
+        dpg.add_button(label="6", width=100, height = 100, callback = calculate, tag ='6')
+        dpg.add_button(label="*", width=100, height = 100, callback = calculate, tag = '*')        
+        dpg.add_button(label="÷", width=100, height = 100, callback = calculate, tag = '/')
 
     with dpg.group(horizontal= True):
-        dpg.add_button(label="4", width=100, height = 80, callback = calculate, tag ='4')
-        dpg.add_button(label="5", width=100, height = 80, callback = calculate, tag ='5')
-        dpg.add_button(label="6", width=100, height = 80, callback = calculate, tag ='6')
-        dpg.add_button(label="-", width=100, height = 80, callback = calculate, tag = '-')
+        dpg.add_button(label="1", width=100, height = 100, callback = calculate, tag = '1')
+        dpg.add_button(label="2", width=100, height = 100,callback = calculate, tag = '2')
+        dpg.add_button(label="3", width=100, height = 100, callback = calculate, tag = '3')
+        dpg.add_button(label="+", width=100, height = 100, callback = calculate, tag = '+')
+        dpg.add_button(label="-", width=100, height = 100, callback = calculate, tag = '-')        
 
     with dpg.group(horizontal= True):
-        dpg.add_button(label="7", width=100, height = 80, callback = calculate, tag = '7')
-        dpg.add_button(label="8", width=100, height = 80, callback = calculate, tag = '8')
-        dpg.add_button(label="9", width=100, height = 80, callback = calculate, tag = '9')
-        dpg.add_button(label="/", width=100, height = 80, callback = calculate, tag = '/')
+        dpg.add_button(label="0", width=100, height = 100, callback = calculate, tag = '0')
+        dpg.add_button(label="00", width=100, height = 100, callback = calculate, tag = '00')
+        dpg.add_button(label=".", width=100, height = 100, callback = calculate, tag = '.')
+        dpg.add_button(label="(", width=100, height = 100, callback = calculate, tag = '(')
+        dpg.add_button(label=")", width=100, height = 100, callback = calculate, tag = ')')
 
     with dpg.group(horizontal= True):
-        delete = dpg.add_button(label="DEL", width=100, height = 80, callback = calculate, tag = 'del')
-        dpg.bind_item_theme(delete, primarybtn_red_theme)
-        dpg.add_button(label="0", width=100, height = 80, callback = calculate, tag = '0')
-        clear = dpg.add_button(label="CLEAR", width=100, height = 80, callback = calculate, tag = 'clear')
-        dpg.bind_item_theme(clear, primarybtn_red_theme)
-        dpg.add_button(label="*", width=100, height = 80, callback = calculate, tag = '*')
-
-    with dpg.group(horizontal= True):
-        equals  = dpg.add_button(label="=", width=316, height = 80, callback = calculate, tag = '=')
+        equals  = dpg.add_button(label="=", width=317, height = 100, callback = calculate, tag = '=')
         dpg.bind_item_theme(equals, primarybtn_green_theme)
-        dpg.add_button(label="%", width=100, height = 80, callback = calculate, tag = '%')
+        delete = dpg.add_button(label="DEL", width=100, height = 100, callback = calculate, tag = 'del')
+        dpg.bind_item_theme(delete, primarybtn_red_theme)
+        clear = dpg.add_button(label="CLEAR", width=100, height = 100, callback = calculate, tag = 'clear')
+        dpg.bind_item_theme(clear, primarybtn_red_theme)
 
-# dpg.setup_dearpygui()
-dpg.set_primary_window("Primary Window", True)
+dpg.set_viewport_title("GUI-Calculator")
+dpg.set_primary_window(main_window, True)
+dpg.set_viewport_resizable(False)
+dpg.set_viewport_width(575)
+dpg.set_viewport_height(605)
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context()
+
 
